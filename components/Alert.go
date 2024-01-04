40aca3f9d706a464f3fa8a6fc8b5319a9c761ce6
numanjvd82 <numanjaved2001@gmail.com>
2024-01-04 22:44:53 +0500

    enhancement: Add alert component ref #2

---
+++ components/Alert.go
@@ -0,0 +1,45 @@
+package components
+
+import (
+	g "github.com/maragudk/gomponents"
+	ghtmx "github.com/maragudk/gomponents-htmx"
+	c "github.com/maragudk/gomponents/components"
+	h "github.com/maragudk/gomponents/html"
+)
+
+type AlertType string
+
+const (
+	AlertSuccess AlertType = "success"
+	AlertError   AlertType = "error"
+	AlertWarning AlertType = "warning"
+)
+
+type AlertProps struct {
+	AlertType AlertType
+	Message   string
+}
+
+func Alert(p *AlertProps) g.Node {
+	classes := c.Classes{
+		"alert": true,
+	}
+
+	if p.AlertType != "" {
+		classes[string(p.AlertType)] = true
+	} else {
+		classes["success"] = true
+	}
+
+	return h.Div(
+		classes,
+		g.Text(p.Message),
+		h.Div(
+			ghtmx.On("click", "htmx.toggleClass(htmx.find('.alert'), 'hide')"),
+			Icon(&IconProps{
+				Identifier: "close",
+			}),
+		),
+		InlineStyle(Assets, "/Alert.css"),
+	)
+}
