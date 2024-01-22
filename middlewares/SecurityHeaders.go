49f12614fe4aad6abf482a6477c013532832c32d
Will Worrall <wcgworrall@gmail.com>
2024-01-22 20:20:40 +0000

    add security headers

---
+++ middlewares/SecurityHeaders.go
@@ -0,0 +1,26 @@
+package middlewares
+
+import (
+	"net/http"
+	"os"
+)
+
+const CSP = "default-src 'self'"
+
+func SecurityHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		goEnv := os.Getenv("GO_ENV")
+
+		if goEnv == "staging" || goEnv == "production" {
+			w.Header().Set("X-XSS-Protection", "1; mode=block")
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.Header().Set("X-Frame-Options", "DENY")
+			w.Header().Set("Content-Security-Policy", CSP)
+			w.Header().Set("Referrer-Policy", "no-referrer")
+			w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
