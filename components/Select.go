1648e4264a1d0fea4135a62bae62bbd34043dded
numanjvd82 <nomanmani62@gmail.com>
2024-03-07 23:14:40 +0500

    refactoring select component

--- components/Select.go
+++ components/Select.go
@@ -1,8 +1,6 @@
 package components
 
 import (
-	"strings"
-
 	g "github.com/maragudk/gomponents"
 	c "github.com/maragudk/gomponents/components"
 	h "github.com/maragudk/gomponents/html"
@@ -19,66 +17,33 @@ type SelectProps struct {
 	Name        string
 	ID          string
 	Multiple    bool
-	DefaultText string
+	Placeholder string
 	Classes     c.Classes
 }
 
-func SelectOptions(o []Option, v []string, name, class, ID string) g.Node {
+func SelectOptions(o []Option, v []string, ID string) g.Node {
 	return h.Ul(
-		h.Class(class),
+		h.Class("select-dropdown"),
 		h.Role("listbox"),
 		h.ID(ID),
 		g.Group(g.Map(o, func(o Option) g.Node {
-			// checked := "false"
-			// for _, val := range v {
-			// 	if val == o.Value {
-			// 		checked = "true"
-			// 		break
-			// 	}
-			// }
-			return h.Li(
-				h.Role("option"),
-				h.Label(
-					g.Text(o.Label),
-					h.Input(
-						h.Name(name),
-						h.Type("radio"),
-						h.Value(o.Value),
-						// g.Attr("checked", checked),
-					),
-				),
-			)
-		})),
-	)
-}
-
-func MultiSelectOptions(o []Option, v []string, name, class, ID string) g.Node {
-	return h.Ul(
-		h.Class(class),
-		h.Role("listbox"),
-		h.ID(ID),
-		g.Group(g.Map(o, func(o Option) g.Node {
-			checked := "false"
+			checked := false
 			for _, val := range v {
 				if val == o.Value {
-					checked = "true"
+					checked = true
+					break
 				}
 			}
+			classes := c.Classes{
+				"option":  true,
+				"checked": checked,
+			}
+
 			return h.Div(
-				h.Class("option"),
-				h.Li(
-					h.Role("option"),
-					h.Label(
-						h.For(o.Value),
-						g.Text(o.Label),
-					),
-					h.Input(
-						h.Name(name),
-						h.Type("checkbox"),
-						h.Value(o.Value),
-						g.Attr("checked", checked),
-					),
-				),
+				classes,
+				h.Role("option"),
+				h.DataAttr("value", o.Value),
+				h.Span(h.Class("option"), g.Text(o.Label)),
 				Icon(&IconProps{
 					Identifier: "check",
 				}),
@@ -106,36 +71,108 @@ func Select(p *SelectProps) g.Node {
 		p.ID = "multi-select"
 	}
 
-	if p.DefaultText == "" && !p.Multiple {
-		p.DefaultText = "Select an option"
-	} else if p.DefaultText == "" && p.Multiple {
-		p.DefaultText = "Select options"
+	if p.Placeholder == "" && !p.Multiple {
+		p.Placeholder = "Select an option"
+	} else if p.Placeholder == "" && p.Multiple {
+		p.Placeholder = "Select options"
 	}
 
 	dropdownId := p.ID + "-dropdown"
 
+	placeholderClasses := c.Classes{
+		"placeholder": true,
+	}
+
+	if len(p.Value) > 0 {
+		placeholderClasses["hidden"] = true
+	}
+
+	optionClasses := c.Classes{
+		"select-value-container": true,
+	}
+
+	// get length of value
+	if len(p.Value) < 1 {
+		optionClasses["hidden"] = true
+	}
+
 	return h.Div(
 		p.Classes,
-		h.DataAttr("default-text", p.DefaultText),
-		g.If(!p.Multiple, h.DataAttr("default-value", p.Value[0])),
-		g.If(p.Multiple, h.DataAttr("default-value", strings.Join(p.Value, ","))),
-		h.ID(p.ID),
+		h.DataAttr("placeholder", p.Placeholder),
+		h.DataAttr("name", p.Name),
 		g.If(p.Multiple, h.DataAttr("multiple", "true")),
-		h.Div(
-			g.If(!p.Multiple, h.Class("select-button")),
-			g.If(p.Multiple, h.Class("multi-select-button")),
+		h.ID(p.ID),
+		h.Button(
+			h.Class("select-button"),
 			h.Role("combobox"),
 			g.If(!p.Multiple, h.Aria("labelledby", "select button")),
 			g.If(p.Multiple, h.Aria("labelledby", "multi-select button")),
 			h.Aria("haspopup", "listbox"),
 			h.Aria("expanded", "false"),
 			h.Aria("controls", dropdownId),
-			g.If(!p.Multiple, h.Span(h.Class("default-value"), g.Text(""))),
-			g.If(p.Multiple, h.Div(h.Class("selected-values"))),
+			g.If(p.Multiple, h.Span(
+				optionClasses,
+				g.Group(g.Map(p.Value, func(v string) g.Node {
+					value := v
+					label := ""
+					for _, o := range p.Options {
+						if o.Value == v {
+							label = o.Label
+							break
+						}
+					}
+					return h.Span(
+						h.Class("selected-value"),
+						h.DataAttr("value", value),
+						g.Text(label),
+						h.Span(
+							h.Class("remove-icon"),
+							g.Text("✕"),
+						),
+						h.Input(
+							h.Type("checkbox"),
+							h.Name(p.Name),
+							h.Value(value),
+							g.Attr("checked", "true"),
+						),
+					)
+				})),
+			)),
+			h.Span(
+				placeholderClasses,
+				h.Class("placeholder"),
+				g.Text(p.Placeholder),
+			),
 			h.Span(h.Class("arrow")),
 		),
-		g.If(!p.Multiple, SelectOptions(p.Options, p.Value, p.Name, "select-dropdown", dropdownId)),
-		g.If(p.Multiple, MultiSelectOptions(p.Options, p.Value, p.Name, "multi-select-dropdown", dropdownId)),
+		g.If(!p.Multiple, SelectOptions(p.Options, p.Value, "select-dropdown")),
+		g.If(p.Multiple, SelectOptions(p.Options, p.Value, dropdownId)),
 		InlineScript("/components/Select.js"),
 	)
 }
+
+// if p.Multiple {
+// 	return h.Span(
+// 		h.Class("selected-value"),
+// 		h.DataAttr("value", v),
+// 		g.Text(v),
+// 		h.Span(
+// 			h.Class("remove-icon"),
+// 			g.Text("✕"),
+// 		),
+// 		h.Input(
+// 			h.Type("checkbox"),
+// 			h.Name(p.Name),
+// 			h.Value(v),
+// 			g.Attr("checked", "true"),
+// 		),
+// 	)
+// }
+
+/*
+<span class="selected-value" data-value="1">
+Option 1
+<span class="remove-icon">✕</span>
+<input type="checkbox" name="select" value="1">
+</span>
+*/
