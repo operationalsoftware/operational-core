21bfabffcd682ed5950cda26b48828fd99e89340
Will Worrall <wcgworrall@gmail.com>
2024-08-06 23:20:22 +0100

    add default behaviour to apppagination

--- internal/apppagination/apppagination.go
+++ internal/apppagination/apppagination.go
@@ -6,9 +6,15 @@ type Pagination struct {
 }
 
 func (p *Pagination) Offset() uint {
+	if p.Page == 0 { // default if not set
+		return 0
+	}
 	return (p.Page - 1) * p.PageSize
 }
 
-func (p *Pagination) Limit() uint {
+func (p *Pagination) Limit(defaultPageSize uint) uint {
+	if p.PageSize == 0 {
+		return defaultPageSize
+	}
 	return p.PageSize
 }
