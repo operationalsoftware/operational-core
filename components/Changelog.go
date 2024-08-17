5439e551719761a8677b959595ca3459bf6581bf
Will Worrall <wcgworrall@gmail.com>
2024-08-17 17:50:26 +0100

    add basic changelog component

---
+++ components/Changelog.go
@@ -0,0 +1,111 @@
+package components
+
+import (
+	"database/sql"
+	"fmt"
+	"reflect"
+	"time"
+
+	g "github.com/maragudk/gomponents"
+	h "github.com/maragudk/gomponents/html"
+	"github.com/shopspring/decimal"
+)
+
+type ChangelogEntry struct {
+	ChangedAt         time.Time
+	ChangedByUsername string
+	IsCreation        bool
+	Changes           map[string]interface{}
+}
+
+type ChangelogFieldDefinition struct {
+	Name  string
+	Label string
+}
+
+func Changelog(entries []ChangelogEntry, fieldDefs []ChangelogFieldDefinition) g.Node {
+	return h.Div(
+		h.H3(g.Text("Changelog")),
+		h.Ul(
+			g.Group(g.Map(entries, func(entry ChangelogEntry) g.Node {
+				return h.Li(
+					h.Div(
+						h.Strong(
+							g.If(
+								entry.IsCreation,
+								g.Text("Created"),
+							),
+							g.If(
+								!entry.IsCreation,
+								g.Text("Changed"),
+							),
+							g.Text(fmt.Sprintf(" by %s at ", entry.ChangedByUsername)),
+							h.Span(h.Class("local-datetime"), g.Text(entry.ChangedAt.Format(time.RFC3339))),
+						),
+					),
+					renderChanges(entry.Changes, fieldDefs),
+				)
+			})),
+		),
+	)
+}
+
+func renderChanges(changes map[string]interface{}, fieldDefs []ChangelogFieldDefinition) g.Node {
+	return h.Ul(
+		g.Group(g.Map(fieldDefs, func(fd ChangelogFieldDefinition) g.Node {
+			if value, exists := changes[fd.Name]; exists {
+				return renderChange(fd.Label, value)
+			}
+			return nil
+		})),
+	)
+}
+
+func renderChange(fieldLabel string, value interface{}) g.Node {
+	formattedValue := formatValue(value)
+	if formattedValue != "" {
+		return h.Li(g.Text(fmt.Sprintf("%s: %s", fieldLabel, formattedValue)))
+	}
+	return nil
+}
+
+func formatValue(value interface{}) string {
+	v := reflect.ValueOf(value)
+
+	switch v.Type() {
+	case reflect.TypeOf(sql.NullString{}):
+		if v.Interface().(sql.NullString).Valid {
+			return v.Interface().(sql.NullString).String
+		}
+	case reflect.TypeOf(sql.NullInt16{}):
+		if v.Interface().(sql.NullInt16).Valid {
+			return fmt.Sprintf("%d", v.Interface().(sql.NullInt16).Int16)
+		}
+	case reflect.TypeOf(sql.NullInt32{}):
+		if v.Interface().(sql.NullInt32).Valid {
+			return fmt.Sprintf("%d", v.Interface().(sql.NullInt32).Int32)
+		}
+	case reflect.TypeOf(sql.NullInt64{}):
+		if v.Interface().(sql.NullInt64).Valid {
+			return fmt.Sprintf("%d", v.Interface().(sql.NullInt64).Int64)
+		}
+	case reflect.TypeOf(sql.NullFloat64{}):
+		if v.Interface().(sql.NullFloat64).Valid {
+			return fmt.Sprintf("%f", v.Interface().(sql.NullFloat64).Float64)
+		}
+	case reflect.TypeOf(sql.NullBool{}):
+		if v.Interface().(sql.NullBool).Valid {
+			return fmt.Sprintf("%t", v.Interface().(sql.NullBool).Bool)
+		}
+	case reflect.TypeOf(decimal.NullDecimal{}):
+		if v.Interface().(decimal.NullDecimal).Valid {
+			return v.Interface().(decimal.NullDecimal).Decimal.String()
+		}
+	case reflect.TypeOf(sql.NullTime{}):
+		if v.Interface().(sql.NullTime).Valid {
+			return v.Interface().(sql.NullTime).Time.Format(time.RFC3339)
+		}
+	}
+
+	return ""
+}
