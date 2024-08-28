e012826e44d409b022d8a6d9afa47b220f9e725f
Will Worrall <wcgworrall@gmail.com>
2024-08-28 18:27:22 +0100

    add Prepare method to SQLExecutor interface

--- internal/db/SQLExecutor.go
+++ internal/db/SQLExecutor.go
@@ -6,4 +6,5 @@ type SQLExecutor interface {
 	Exec(query string, args ...any) (sql.Result, error)
 	Query(query string, args ...any) (*sql.Rows, error)
 	QueryRow(query string, args ...any) *sql.Row
+	Prepare(query string) (*sql.Stmt, error)
 }
