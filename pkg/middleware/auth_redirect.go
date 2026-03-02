9b792e65d426758b26428e8f839e98441f6e9d4e
Will Worrall <wcgworrall@gmail.com>
2026-03-02 21:49:47 +0000

    fix(middleware): return 401 for unauthenticated api routes

--- pkg/middleware/auth_redirect.go
+++ pkg/middleware/auth_redirect.go
@@ -4,6 +4,7 @@ import (
 	"app/internal/model"
 	"app/pkg/reqcontext"
 	"net/http"
+	"strings"
 )
 
 func AuthRedirect(next http.Handler) http.Handler {
@@ -15,6 +16,10 @@ func AuthRedirect(next http.Handler) http.Handler {
 
 		_, ok := r.Context().Value(reqcontext.ReqContextKeyUser).(model.User)
 		if !ok {
+			if strings.HasPrefix(r.URL.Path, "/api/") {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
 			http.Redirect(w, r, "/auth/password", http.StatusSeeOther)
 			return
 		}
